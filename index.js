bc0a38acab0c717dbf29b2bd7eb822591d11f818
Kelly Ng <kellyng19lover@gmail.com>
2023-10-10 10:20:48 -0400

    3t

---
+++ index.js
@@ -0,0 +1,18 @@
+const eat = num(prompt ("breakfast or lunch?"));
+
+
+const food = [{
+   
+    breakfast:["eggs", "bacon", "pancakes", "waffles"],
+    yummy : false,
+}
+{  
+    Lunch: ["pizza", "sushi", "pasta", "sandwhiches"]
+    yummy : true,
+
+}]
+
+food.forEach((food)=>console.log (food)
+
+const food= breakfast.filter((students)=> students.graduated === false);
+console.log(graduated);
